fe1a1ec9e51c9c3ecdf60d7db010d1ab8e83e492
durexex <eduardodurieuxlopes@gmail.com>
2022-02-05 22:08:33 -0300

    feign cloud

--- hr-payroll/src/main/java/com/devdurex/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devdurex/payroll/services/PaymentService.java
@@ -1,32 +1,22 @@
 package com.devdurex.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devdurex.payroll.entities.Payment;
 import com.devdurex.payroll.entities.Worker;
+import com.devdurex.payroll.feignClients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-
-	@Value("${hr-worker.host}")
-	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
 		
-		Map<String, String> uriVariables = new HashMap<>(); 
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
