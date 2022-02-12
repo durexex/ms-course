a15867b45e8e9bb86e911d53ed32baa09debd040
durexex <eduardodurieuxlopes@gmail.com>
2022-02-12 17:04:14 -0300

    zuul config for users

--- hr-user/src/main/java/com/devdurex/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devdurex/hruser/HrUserApplication.java
@@ -23,5 +23,7 @@ public class HrUserApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
 	}
+	
+	
 
 }
