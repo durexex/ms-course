78a25463ac07f9320eae7f7cd112e5dda8beb48e
durexex <eduardodurieuxlopes@gmail.com>
2022-02-13 10:18:46 -0300

    login jwt token generated

--- hr-oauth/src/main/java/com/devdurex/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devdurex/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.devdurex.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devdurex.hroauth.entities.User;
 import com.devdurex.hroauth.feignClients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class); 
 	
@@ -23,6 +26,18 @@ public class UserService {
 			throw new IllegalArgumentException("email not found");
 		}
 		logger.info("Email found: " + email);
+				
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		logger.error("Email not found: " + username);
+		if (user == null) {
+			throw new UsernameNotFoundException("email not found");
+		}
+		logger.info("Email found: " + username);
 				
 		return user;
 	}
